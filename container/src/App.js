fa37a9b2dda5f98884a86a66960969a3751e2d90
Jose Martinez <jose.martinezmayora@externos-cl.cencosud.com>
2022-11-29 08:55:39 -0300

    first commit

---
+++ container/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MarketingApp from './components/MarketingApp'
+
+export default () => (
+  <>
+    <div>hi there!</div>
+    <hr></hr>
+    <MarketingApp/>
+  </>
+)
